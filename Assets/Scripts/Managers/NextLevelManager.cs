8b5fdccc177482175dd575379244f033047a71d2
SubjectFrost <breu.lorenz@gmail.com>
2015-01-22 18:38:52 -0500

    Game
    Third person shooter game, modified from a tutorial. Added a "second
    level" at 50% health, as well as a game start splash screen.

---
+++ Assets/Scripts/Managers/NextLevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class NextLevelManager : MonoBehaviour
+{
+	public PlayerHealth playerHealth;
+	
+	
+	Animator anim;
+	
+	
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	
+	void Update()
+	{
+		if (playerHealth.currentHealth <= 50)
+		{
+			anim.SetTrigger("NextLevel");
+		}
+	}
+}
